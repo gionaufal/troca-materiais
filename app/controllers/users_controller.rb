5e5b11dc8df181adf087c583ed9cad6e7c833cc9
Giovanni Naufal <gionaufal@gmail.com>
2017-05-03 18:14:18 -0300

    Add matches to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,37 @@ class UsersController < ApplicationController
     @user = UserPresenter.new(@user, current_user)
     @wishes = @user.wishes
     @materials = @user.materials
+    @wishes_matches = find_wishes_matches
+    @materials_matches = find_materials_matches
   end
 
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def find_wishes_matches
+    wishes = []
+    @wishes.each do |wish|
+      wishes << wish.product
+    end
+    matches = []
+    wishes.each do |wish|
+      matches << Material.where(product: wish).to_a
+    end
+    matches.flatten
+  end
+
+  def find_materials_matches
+    materials = []
+    @materials.each do |material|
+      materials << material.product
+    end
+    matches = []
+    materials.each do |material|
+      matches << Wish.where(product: material).to_a
+    end
+    matches
   end
 end
